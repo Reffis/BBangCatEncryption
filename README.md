428164baaaf0a1dd9e5f43830ff59673656ec515
fn79 <74909209+fn79@users.noreply.github.com>
2021-07-26 23:01:43 +0900

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BBangCatEncryption
+
+ㅇㅇ
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod bce {
+    enum ErrorType {
+
+    }
+    mod to_bce {
+        pub fn new(text: &str) -> Result<&str, crate::bce::ErrorType> {
+            // TODO
+
+        }
+    }
+}
