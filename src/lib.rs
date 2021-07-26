b3613edf76370c1a3e80a110b500343a2e7d1a44
fn79 <74909209+fn79@users.noreply.github.com>
2021-07-26 23:51:41 +0900

    소문자

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,40 @@
+mod converter;
+
 mod bce {
-    enum ErrorType {
+    pub mod to_bce {
+        use crate::converter::{get_key_to_bce};
 
+        pub fn new(text: &str) -> String {
+            let mut result = String::new();
+            for i in text.split("").collect::<Vec<&str>>() {
+                match get_key_to_bce(i) {
+                    Some(e) => result.push_str(e),
+                    None => result.push_str(i),
+                }
+            }
+            result
+        }
     }
-    mod to_bce {
-        pub fn new(text: &str) -> Result<&str, crate::bce::ErrorType> {
-            // TODO
 
+    pub mod to_str {
+        use crate::converter::{get_key_to_str};
+
+        pub fn new(text: &str) -> String {
+            let mut result = String::new();
+            for i in text.split("").collect::<Vec<&str>>() {
+                match get_key_to_str(i) {
+                    Some(e) => result.push_str(e),
+                    None => result.push_str(i),
+                }
+            }
+            result
         }
     }
 }
+    mod test {
+        #[test]
+        fn test() {
+            println!("{}", crate::bce::to_bce::new("hello, world!"));
+            println!("{}", crate::bce::to_str::new("빵뺭뺗뻭뿅뿡캣캤컜"));
+        }
+    }
