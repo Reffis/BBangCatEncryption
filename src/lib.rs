cb45ae49835fdedc2413ca3f392c44da930cbc81
fn79 <74909209+fn79@users.noreply.github.com>
2021-07-27 15:52:37 +0900

    A

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod converter;
 
 mod bce {
     pub mod to_bce {
-        use crate::converter::{get_key_to_bce};
+        use crate::converter::get_key_to_bce;
 
         pub fn new(text: &str) -> String {
             let mut result = String::new();
@@ -17,7 +17,7 @@ mod bce {
     }
 
     pub mod to_str {
-        use crate::converter::{get_key_to_str};
+        use crate::converter::get_key_to_str;
 
         pub fn new(text: &str) -> String {
             let mut result = String::new();
@@ -31,10 +31,11 @@ mod bce {
         }
     }
 }
-    mod test {
-        #[test]
-        fn test() {
-            println!("{}", crate::bce::to_bce::new("hello, world!"));
-            println!("{}", crate::bce::to_str::new("빵뺭뺗뻭뿅뿡캣캤컜"));
-        }
+mod test {
+    #[test]
+    fn test() {
+        use crate::bce::{to_str, to_bce};
+        assert_eq!(to_bce::new("a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"),
+                   to_bce::new(to_str::new("빵 뽷 삫 뺗 빽 뻭 뺶 뻏 켓 캣 컛 콋 켔 콌 컜 뽕 뿡 뿅 뺭 뺑 쁑 뺭 삉 쀵 뽱 뼝 뿾 쀇 뽺 뾹 뾂 쁅 뾾 뿢 쁥 뾹 쀇 뾳 뼿 뾎 뽬 쾫 켥 캛 컎 켦 캺 켎 쾛 컱 콇 켈").as_str()));
     }
+}
